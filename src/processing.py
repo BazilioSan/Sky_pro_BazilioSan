2475b32b675b0a5e1ac3489b25dd84fbe1021802
Alexandr <blix@list.ru>
2024-06-04 00:12:04 +0300

    Добавлен модуль processing.py после линтерной корректировки

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,20 @@
-def filter_by_state(list_of_data, state="CANCELED"):
+from typing import Iterable
+
+
+def filter_by_state(list_of_data: dict, state: str = "CANCELED") -> Iterable[dict]:
     """Функция, возвращающая список словарей с указанным состоянием."""
 
     result = []
-    for dict in list_of_data:
-        if dict["state"] == state:
-            result.append(dict)
+    for dictionary in list_of_data:
+        if dictionary["state"] == state:
+            result.append(dictionary)
 
     return result
 
-def sort_by_date(list_of_dicts, order = True):
+
+def sort_by_date(list_of_dicts: dict, order: bool = True) -> Iterable[dict]:
     """Функция, возвращающая список словарей, отсортированных по дате."""
 
     result = sorted(list_of_dicts, key=lambda x: x["date"], reverse=order)
 
     return result
-
