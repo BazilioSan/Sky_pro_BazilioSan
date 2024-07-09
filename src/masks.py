8505bfa222149cadee0f308cfd498dc32fa62096
Alexandr <blix@list.ru>
2024-07-09 23:45:23 +0300

    Удален пакет utils модуль transactions.py переименован в utils.py и перемещен в src добавлено логгирование для модулей utils.py и masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,23 @@
 from typing import Union
+import logging
+import os
+
+logs_dir = os.path.join(os.path.dirname(__file__), "..", 'logs')
+if not os.path.exists(logs_dir):
+    os.makedirs(logs_dir)
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("logs/masks.log")
+file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_card_mask(card_number: Union[str, int]) -> str:
     """Функция маскировки части номера карты звездочками"""
+
+    logger.info(f"Функция get_card_mask запущена с входным номером: {card_number}")
 
     mask_number: list = []
 
@@ -11,6 +26,8 @@ def get_card_mask(card_number: Union[str, int]) -> str:
     card_parts = list(format_number)
 
     # Срезами разделяет номер на части
+    logger.info(f"Преобразовывает номер карты")
+
     first_part = card_parts[0:4]
     second_part = card_parts[4:6]
     second_part.append("**")
@@ -22,6 +39,7 @@ def get_card_mask(card_number: Union[str, int]) -> str:
     mask_number.append("".join(second_part))
     mask_number.append("".join(third_part))
     mask_number.append("".join(forth_part))
+    logger.info(f"функция get_card_mask успешно отработала с результатом: {mask_number}")
 
     return " ".join(mask_number)
 
@@ -29,14 +47,21 @@ def get_card_mask(card_number: Union[str, int]) -> str:
 def get_account_mask(account_number: Union[str, int]) -> str:
     """Функция маскировки части счета звездочками"""
 
+    logger.info(f"Функция get_account_mask запущена с входным номером: {account_number}")
+
     mask_number: list = ["**"]
 
     # Преобразовывает ввод в строку (если введено просто число), а потом в список
+
     format_number = str(account_number)
     account_parts = list(format_number)
 
     # Делает срез последних 4 чисел счета и добавляет их в результат
     first_part = account_parts[-4:]
     mask_number.append("".join(first_part))
-
+    logger.info(f"Функция get_account_mask успешно отработала с результатом: {mask_number}")
     return "".join(mask_number)
+
+
+a = get_account_mask(73654108430135874305)
+print(a)
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,21 @@
 import json
+import logging
 import os
 
-from utils.external_api import convert_currency
+from src.external_api import convert_currency
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("logs/utils.log")
+file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_transactions_json(file_path: str = None) -> list:
     """Функция чтения и парсинга файла с транзакциями"""
+
+    logger.info(f"Запуск функции. Чтение файла {file_path}")
 
     if file_path is None:
         base_dir = os.getcwd()
@@ -20,13 +30,17 @@ def get_transactions_json(file_path: str = None) -> list:
             if type(data) == list:
                 return data
             else:
+                logger.error("Ошибка. Файл не содержит список данных")
                 raise ValueError("Файл не содержит список данных")
 
     except FileNotFoundError:
+        logger.error("Ошибка. Файл не найден")
         return []
     except json.JSONDecodeError:
+        logger.error("Ошибка. Ошибка при чтении файла")
         raise ValueError("Ошибка при чтении файла")
     except ValueError as e:
+        logger.error(f"Ошибка: {e}")
         print(e)
 
         return []
@@ -35,6 +49,7 @@ def get_transactions_json(file_path: str = None) -> list:
 def get_transaction_amount(transactions: list, transaction_id: int):
     """Функция конвертации конкретной транзакции"""
 
+    logger.info(f"Запуск функции. Поиск транзакции с id {transaction_id}")
     for transaction in transactions:
         if transaction.get("id") == transaction_id:
             if transaction["operationAmount"]["currency"]["code"] == "RUB":
@@ -46,12 +61,14 @@ def get_transaction_amount(transactions: list, transaction_id: int):
                 transaction_convert["currency"] = transaction["operationAmount"]["currency"]["code"]
                 rub_amount = round(convert_currency(transaction_convert), 2)
                 if rub_amount != 0:
+                    logger.info(f"Функция успешно отработала с результатом {rub_amount}")
                     return rub_amount
                 else:
+                    logger.error("Ошибка. Конвертация не совершена")
                     return "Конвертация не может быть выполнена"
     else:
+        logger.error("Ошибка. Транзакция не найдена")
         return "Транзакция не найдена"
-
 
 # if __name__ == "__main__":
 #     transactions = get_transactions_json("../data/operations.json")
